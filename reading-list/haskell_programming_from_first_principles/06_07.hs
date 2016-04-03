aefe684c6f2b386054e37056a93dc19e2b6e5e73
matt <matthewjhamrick@gmail.com>
2016-04-03 09:55:41 -0500

    .

---
+++ reading-list/haskell_programming_from_first_principles/06_07.hs
@@ -0,0 +1,11 @@
+-- 1. max (length [1, 2, 3]) (length [8, 9, 10, 11, 12])
+-- 5
+
+-- 2. compare (3 * 4) (3 * 5)
+-- LT
+
+-- 3. compare "Julie" True
+-- won't work.
+
+-- 4. (5 + 3) > (3 + 6)
+-- False
