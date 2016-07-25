daa4452ffc3ddad344e579f50e478f803ae02420
Matt Hamrick <matt.hamrick.kbvv@statefarm.com>
2016-07-25 06:49:09 -0500

    finished a few more chapters

--- reading-list/mastering_clojure_macros/05_speed_up_your_systems.clj
+++ reading-list/mastering_clojure_macros/05_speed_up_your_systems.clj
@@ -1,2 +1,12 @@
-(ns )
+(ns speed-up-your-systems
+  (:require [criterium.core :as crit]))
 
+(defn length-1 [x] (.length x))
+
+(crit/with-progress-reporting
+  (crit/bench (length-1 "hi there!")))
+
+(defn length-2 [^String x] (.length x))
+
+(crit/with-progress-reporting
+  (crit/bench (length-2 "hi there!")))
