c54ff52442c38806eeb81f602da00f45b2f159aa
matt <matthewjhamrick@gmail.com>
2016-10-03 18:57:11 -0500

    Started day 3 of Erlang.

---
+++ reading-list/seven_languages_in_seven_weeks/erlang/day_3.erl
@@ -0,0 +1,29 @@
+-module(day_3).
+
+%% Day 3 Self-Study
+
+%% These exercises are relatively easy, but I did add some bonus questions to
+%% stretch you a little bit.
+
+%% Open Telecom Platform (OTP) is a powerful package with much of what you’ll
+%% need to build a distributed, concurrent service.
+
+%% Find:
+
+%% An OTP service that will restart a process if it dies
+
+%% Documentation for building a simple OTP server
+
+%% Do:
+
+%% Monitor the translate_service and restart it should it die.
+
+%% Make the Doctor process restart itself if it should die.
+
+%% Make a monitor for the Doctor monitor. If either monitor dies, restart it.
+
+%% The following bonus questions will take a little bit of research to complete:
+
+%% Create a basic OTP server that logs messages to a file.
+
+%% Make the translate_service work across a network.
