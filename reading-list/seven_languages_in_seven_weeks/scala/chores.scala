2ece09ca6a8f2763d272a8d198c2adb8c5fdb92c
Matt Hamrick <matt.hamrick.kbvv@statefarm.com>
2016-10-10 08:23:21 -0500

    Did day 3 of scala

---
+++ reading-list/seven_languages_in_seven_weeks/scala/chores.scala
@@ -0,0 +1,10 @@
+println("Showing off some pattern matching")
+
+def doChore(chore: String): String = chore match {
+  case "clean dishes" => "scrub, dry"
+  case "cook dinner" => "chop, sizzle"
+  case _ => "whine, complain"
+}
+
+println(doChore("clean dishes"))
+println(doChore("mow lawn"))
