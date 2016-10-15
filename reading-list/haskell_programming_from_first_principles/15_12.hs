3be9dfa85b4c57fed36ec231413958dcd3a5868e
matt <matthewjhamrick@gmail.com>
2016-10-15 12:50:05 -0500

    Cleaned up a haskell book exercise

--- reading-list/haskell_programming_from_first_principles/15_12.hs
+++ reading-list/haskell_programming_from_first_principles/15_12.hs
@@ -2,6 +2,15 @@ import Control.Monad
 import Data.Monoid
 import Test.QuickCheck
 
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
 data Optional a =
   Nada
   |Only a
@@ -17,13 +26,16 @@ instance Monoid (First' a) where
   (First' Nada) `mappend` a@(First' (Only _)) = a
   a@(First' Nada) `mappend` (First' Nada) = a
 
-firstMappend :: First' a -> First' a -> First' a
-firstMappend = mappend
+instance Arbitrary a => Arbitrary (Optional a) where
+  arbitrary = frequency [ (1, return Nada)
+                        , (3, fmap Only arbitrary)]
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary = fmap First' arbitrary
 
 type FirstMappend = First' String -> First' String -> First' String -> Bool
 
-main :: IO () main =
-  do
-    quickCheck (monoidAssoc :: FirstMappend)
-    quickCheck (monoidLeftIdentity :: First' String -> Bool)
-    quickCheck (monoidRightIdentity :: First' String -> Bool)
+main :: IO ()
+main = do quickCheck (monoidAssoc :: FirstMappend)
+          quickCheck (monoidLeftIdentity :: First' String -> Bool)
+          quickCheck (monoidRightIdentity :: First' String -> Bool)
