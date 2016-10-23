44fd172a84f5f6753a5b84a87cd7f5c1b5060f25
matt <matthewjhamrick@gmail.com>
2016-10-22 19:52:28 -0500

    working on state

--- reading-list/haskell_programming_from_first_principles/23_05.hs
+++ reading-list/haskell_programming_from_first_principles/23_05.hs
@@ -1,33 +1,9 @@
+import Control.Applicative (liftA3)
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State
 import System.Random
 
-rollsToGetTwenty :: StdGen -> Int
-rollsToGetTwenty g = go 0 0 g
-  where go :: Int -> Int -> StdGen -> Int
-        go sum count gen
-          | sum >= 20 = count
-          | otherwise = let (die, nextGen) = randomR (1, 6) gen
-                        in go (sum + die) (count + 1) nextGen
-
--- 1. Refactor rollsToGetTwenty into having the limit be a function argument.
-
-rollsToGetN :: Int -> StdGen -> Int
-rollsToGetN n g = go 0 0 g
-  where go :: Int -> Int -> StdGen -> Int
-        go sum count gen
-          | sum >= n = count
-          | otherwise = let (die, nextGen) = randomR (1, 6) gen
-                        in go (sum + die) (count + 1) nextGen
-
--- 2. Change rollsToGetN to recording the series of die that occurred in
--- addition to the count.
-data Die =
-  DieOne
-  | DieTwo
-  | DieThree
-  | DieFour
-  | DieFive
-  | DieSix
-  deriving (Eq, Show)
+data Die = DieOne | DieTwo | DieThree | DieFour | DieFive | DieSix deriving (Eq, Show)
 
 intToDie :: Int -> Die
 intToDie n = case n of
@@ -40,11 +16,45 @@ intToDie n = case n of
                -- Use this tactic _extremely_ sparingly.
                x -> error $ "intToDie got non 1-6 integer: " ++ show x
 
+rollDieThreeTimes :: (Die, Die, Die)
+rollDieThreeTimes = do let s = mkStdGen 0
+                           (d1, s1) = randomR (1, 6) s
+                           (d2, s2) = randomR (1, 6) s1
+                           (d3, s3) = randomR (1, 6) s2
+                       (intToDie d1, intToDie d2, intToDie d3)
+
+rollDie :: State StdGen Die
+rollDie = state $ do (n, s) <- randomR (1, 6)
+                     return (intToDie n, s)
+
+rollDie' :: State StdGen Die
+rollDie' = intToDie <$> state (randomR (1, 6))
+
+rollDieThreeTimes' :: State StdGen (Die, Die, Die)
+rollDieThreeTimes' = liftA3 (,,) rollDie rollDie rollDie
+
+nDie :: Int -> State StdGen [Die]
+nDie n = replicateM n rollDie
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty g = go 0 0 g
+  where go :: Int -> Int -> StdGen -> Int
+        go sum count gen
+          | sum >= 20 = count
+          | otherwise = let (die, nextGen) = randomR (1, 6) gen
+                        in go (sum + die) (count + 1) nextGen
+
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN n g = go 0 0 g
+  where go sum count gen
+          | sum >= n = count
+          | otherwise = let (die, nextGen) = randomR (1, 6) gen
+                        in go (sum + die) (count + 1) nextGen
+
 rollsCountLogged :: Int -> StdGen -> (Int, [Die])
-rollsCountLogged n g = go 0 0 [] g
-  where go :: Int -> Int -> [Die] -> StdGen -> (Int, [Die])
-        go sum count dice gen
-          | sum >= n = (count, dice)
-          | otherwise = let (dieNum, nextGen) = randomR (1, 6) gen
-                            die = (intToDie dieNum)
-                        in go (sum + dieNum) (count + 1) (die:dice) nextGen
+rollsCountLogged n g = go 0 (0, []) g
+  where go sum thusFar gen
+          | sum >= n = thusFar
+          | otherwise = let (die, nextGen) = randomR (1, 6) gen
+                            (count, dice) = thusFar
+                        in go (sum + die) (count + 1, intToDie die:dice) nextGen
