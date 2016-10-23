204fa6a10df5bcc4fcf3080c75561ad5574c9290
matt <matthewjhamrick@gmail.com>
2016-10-22 20:14:03 -0500

    wrote state functor, applicative, and monad
    And I think I even understood what I was doing!!!

--- reading-list/haskell_programming_from_first_principles/23_06.hs
+++ reading-list/haskell_programming_from_first_principles/23_06.hs
@@ -5,9 +5,28 @@ newtype Moi s a = Moi { runMoi :: s -> (a, s) }
 -- Implement the Functor instance for State.
 
 instance Functor (Moi s) where
-  -- The Moi g here is of type s -> (a, s)
-  --                           a -> b
   fmap :: (a -> b) -> Moi s a -> Moi s b
-  fmap f (Moi g) = Moi
+  fmap f (Moi g) = Moi $ \s -> let (a, s2) = g s
+                               in (f a, s2)
 
--- Prelude> runMoi ((+1) <$> (Moi $ \s -> (0, s))) 0 (1,0)
+-- Prelude> runMoi ((+1) <$> (Moi $ \s -> (0, s))) 0
+-- (1,0)
+
+-- State Applicative
+-- Write the Applicative instance for State.
+instance Applicative (Moi s) where
+  pure :: a -> Moi s a
+  pure a = Moi $ \s -> (a, s)
+  (<*>) :: Moi s (a -> b) -> Moi s a -> Moi s b
+  (Moi f) <*> (Moi g) = Moi $ \s -> let (fab, s2) = f s
+                                        (a, _) = g s
+                                    in (fab a, s2)
+
+-- State Monad
+-- Write the Monad instance for State.
+instance Monad (Moi s) where
+  return = pure
+  (>>=) :: Moi s a -> (a -> Moi s b) -> Moi s b
+  (Moi f) >>= g = Moi $ \s -> let (a, s2) = f s
+                                  (Moi sb) = g a
+                              in sb s
