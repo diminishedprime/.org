e3b7589084649268fbcaf3900ba6e78f18b141e9
matt <matthewjhamrick@gmail.com>
2016-10-28 14:07:57 -0500

    twinplicitive

--- reading-list/haskell_programming_from_first_principles/25_04.hs
+++ reading-list/haskell_programming_from_first_principles/25_04.hs
@@ -1,8 +1,9 @@
-newtype Compose f g a =
-  Compose {getCompose :: f (g a)}
-  deriving (Eq, Show)
+newtype Compose f g a = Compose {getCompose :: f (g a)} deriving (Eq, Show)
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
-  pure = Compose $ f g
-  (Compose f) <*> (Compose g) = undefined
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) =
+    Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure = Compose . pure . pure
+  (Compose f) <*> (Compose a) = Compose $ (<*>) <$> f <*> a
