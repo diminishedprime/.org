6944a55bbc8937012fde0d9a74a6ef7a955cc1f7
Matt Hamrick <matt.hamrick.kbvv@statefarm.com>
2017-06-07 15:17:21 -0500

    reformatted code

--- programmey_stuff/coursera/machine_learning/week_8/machine-learning-ex7/ex7/ex7_pca.m
+++ programmey_stuff/coursera/machine_learning/week_8/machine-learning-ex7/ex7/ex7_pca.m
@@ -27,11 +27,11 @@ clear ; close all; clc
 %
 fprintf('Visualizing example dataset for PCA.\n\n');
 
-%  The following command loads the dataset. You should now have the 
-%  variable X in your environment
+            %  The following command loads the dataset. You should now have the
+            %  variable X in your environment
 load ('ex7data1.mat');
 
-%  Visualize the example dataset
+                                %  Visualize the example dataset
 plot(X(:, 1), X(:, 2), 'bo');
 axis([0.5 6.5 2 8]); axis square;
 
@@ -45,16 +45,16 @@ pause;
 %
 fprintf('\nRunning PCA on example dataset.\n\n');
 
-%  Before running PCA, it is important to first normalize X
+                     %  Before running PCA, it is important to first normalize X
 [X_norm, mu, sigma] = featureNormalize(X);
 
-%  Run PCA
+                                %  Run PCA
 [U, S] = pca(X_norm);
 
-%  Compute mu, the mean of the each feature
+                                %  Compute mu, the mean of the each feature
 
-%  Draw the eigenvectors centered at mean of data. These lines show the
-%  directions of maximum variations in the dataset.
+         %  Draw the eigenvectors centered at mean of data. These lines show the
+         %  directions of maximum variations in the dataset.
 hold on;
 drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
 drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
@@ -69,20 +69,20 @@ pause;
 
 
 %% =================== Part 3: Dimension Reduction ===================
-%  You should now implement the projection step to map the data onto the 
-%  first k eigenvectors. The code will then plot the data in this reduced 
-%  dimensional space.  This will show you what the data looks like when 
+%  You should now implement the projection step to map the data onto the
+%  first k eigenvectors. The code will then plot the data in this reduced
+%  dimensional space.  This will show you what the data looks like when
 %  using only the corresponding eigenvectors to reconstruct it.
 %
 %  You should complete the code in projectData.m
 %
 fprintf('\nDimension reduction on example dataset.\n\n');
 
-%  Plot the normalized dataset (returned from pca)
+                              %  Plot the normalized dataset (returned from pca)
 plot(X_norm(:, 1), X_norm(:, 2), 'bo');
 axis([-4 3 -4 3]); axis square
 
-%  Project the data onto K = 1 dimension
+                                %  Project the data onto K = 1 dimension
 K = 1;
 Z = projectData(X_norm, U, K);
 fprintf('Projection of the first example: %f\n', Z(1));
@@ -92,11 +92,11 @@ X_rec  = recoverData(Z, U, K);
 fprintf('Approximation of the first example: %f %f\n', X_rec(1, 1), X_rec(1, 2));
 fprintf('\n(this value should be about  -1.047419 -1.047419)\n\n');
 
-%  Draw lines connecting the projected points to the original points
+            %  Draw lines connecting the projected points to the original points
 hold on;
 plot(X_rec(:, 1), X_rec(:, 2), 'ro');
 for i = 1:size(X_norm, 1)
-    drawLine(X_norm(i,:), X_rec(i,:), '--k', 'LineWidth', 1);
+  drawLine(X_norm(i,:), X_rec(i,:), '--k', 'LineWidth', 1);
 end
 hold off
 
@@ -109,10 +109,10 @@ pause;
 %
 fprintf('\nLoading face dataset.\n\n');
 
-%  Load Face dataset
+                                %  Load Face dataset
 load ('ex7faces.mat')
 
-%  Display the first 100 faces in the dataset
+                                %  Display the first 100 faces in the dataset
 displayData(X(1:100, :));
 
 fprintf('Program paused. Press enter to continue.\n');
@@ -123,16 +123,16 @@ pause;
 %  We display the first 36 eigenfaces.
 %
 fprintf(['\nRunning PCA on face dataset.\n' ...
-         '(this might take a minute or two ...)\n\n']);
+           '(this might take a minute or two ...)\n\n']);
 
-%  Before running PCA, it is important to first normalize X by subtracting 
-%  the mean value from each feature
+     %  Before running PCA, it is important to first normalize X by subtracting
+     %  the mean value from each feature
 [X_norm, mu, sigma] = featureNormalize(X);
 
-%  Run PCA
+                                %  Run PCA
 [U, S] = pca(X_norm);
 
-%  Visualize the top 36 eigenvectors found
+                                %  Visualize the top 36 eigenvectors found
 displayData(U(:, 1:36)');
 
 fprintf('Program paused. Press enter to continue.\n');
@@ -140,8 +140,8 @@ pause;
 
 
 %% ============= Part 6: Dimension Reduction for Faces =================
-%  Project images to the eigen space using the top k eigenvectors 
-%  If you are applying a machine learning algorithm 
+%  Project images to the eigen space using the top k eigenvectors
+%  If you are applying a machine learning algorithm
 fprintf('\nDimension reduction for face dataset.\n\n');
 
 K = 100;
@@ -154,7 +154,7 @@ fprintf('\n\nProgram paused. Press enter to continue.\n');
 pause;
 
 %% ==== Part 7: Visualization of Faces after PCA Dimension Reduction ====
-%  Project images to the eigen space using the top K eigen vectors and 
+%  Project images to the eigen space using the top K eigen vectors and
 %  visualize only using those K dimensions
 %  Compare to the original input, which is also displayed
 
@@ -163,13 +163,13 @@ fprintf('\nVisualizing the projected (reduced dimension) faces.\n\n');
 K = 100;
 X_rec  = recoverData(Z, U, K);
 
-% Display normalized data
+                                % Display normalized data
 subplot(1, 2, 1);
 displayData(X_norm(1:100,:));
 title('Original faces');
 axis square;
 
-% Display reconstructed data from only k eigenfaces
+                             % Display reconstructed data from only k eigenfaces
 subplot(1, 2, 2);
 displayData(X_rec(1:100,:));
 title('Recovered faces');
@@ -181,36 +181,36 @@ pause;
 
 %% === Part 8(a): Optional (ungraded) Exercise: PCA for Visualization ===
 %  One useful application of PCA is to use it to visualize high-dimensional
-%  data. In the last K-Means exercise you ran K-Means on 3-dimensional 
+%  data. In the last K-Means exercise you ran K-Means on 3-dimensional
 %  pixel colors of an image. We first visualize this output in 3D, and then
 %  apply PCA to obtain a visualization in 2D.
 
 close all; close all; clc
 
-% Reload the image from the previous exercise and run K-Means on it
-% For this to work, you need to complete the K-Means assignment first
+           % Reload the image from the previous exercise and run K-Means on it
+           % For this to work, you need to complete the K-Means assignment first
 A = double(imread('bird_small.png'));
 
-% If imread does not work for you, you can try instead
-%   load ('bird_small.mat');
+                          % If imread does not work for you, you can try instead
+                          %   load ('bird_small.mat');
 
 A = A / 255;
 img_size = size(A);
 X = reshape(A, img_size(1) * img_size(2), 3);
-K = 16; 
+K = 16;
 max_iters = 10;
 initial_centroids = kMeansInitCentroids(X, K);
 [centroids, idx] = runkMeans(X, initial_centroids, max_iters);
 
-%  Sample 1000 random indexes (since working with all the data is
-%  too expensive. If you have a fast computer, you may increase this.
+           %  Sample 1000 random indexes (since working with all the data is
+           %  too expensive. If you have a fast computer, you may increase this.
 sel = floor(rand(1000, 1) * size(X, 1)) + 1;
 
-%  Setup Color Palette
+                                %  Setup Color Palette
 palette = hsv(K);
 colors = palette(idx(sel), :);
 
-%  Visualize the data and centroid memberships in 3D
+                            %  Visualize the data and centroid memberships in 3D
 figure;
 scatter3(X(sel, 1), X(sel, 2), X(sel, 3), 10, colors);
 title('Pixel dataset plotted in 3D. Color shows centroid memberships');
@@ -220,14 +220,14 @@ pause;
 %% === Part 8(b): Optional (ungraded) Exercise: PCA for Visualization ===
 % Use PCA to project this cloud to 2D for visualization
 
-% Subtract the mean to use PCA
+                                % Subtract the mean to use PCA
 [X_norm, mu, sigma] = featureNormalize(X);
 
-% PCA and project the data to 2D
+                                % PCA and project the data to 2D
 [U, S] = pca(X_norm);
 Z = projectData(X_norm, U, 2);
 
-% Plot in 2D
+                                % Plot in 2D
 figure;
 plotDataPoints(Z(sel, :), idx(sel), K);
 title('Pixel dataset plotted in 2D, using PCA for dimensionality reduction');
